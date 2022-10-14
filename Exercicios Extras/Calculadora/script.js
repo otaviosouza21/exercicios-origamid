8932662771ddbb96736d2e70dfbc0b9acf8102cd
Otavio Souza <otaviosouzalu@gmail.com>
2022-10-14 00:12:54 -0300

    novo

---
+++ Exercicios Extras/Calculadora/script.js
@@ -0,0 +1,50 @@
+/*function insert(num) {
+  var numero = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = numero + num;
+}
+function clean(){
+  document.getElementById("resultado").innerHTML = " ";
+}
+
+function limpa(){
+    var resultado = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1);
+
+}
+
+function calcula(){
+  var resultado = document.getElementById("resultado").innerHTML;
+
+  if(resultado){
+    document.getElementById("resultado").innerHTML = eval(resultado);
+  }
+else{
+  document.getElementById('resuldado').innerHTML = ("Nada")
+}
+}
+*/
+function insert(num){
+  var numero = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = numero + num;
+}
+
+function clean(){
+  document.getElementById("resultado").innerHTML = " ";
+}
+
+function limpa(){
+  var resultado = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcula(){
+  var resultado = document.getElementById("resultado").innerHTML;
+
+  if(resultado){
+    document.getElementById("resultado").innerHTML = eval(resultado);
+    
+  }
+else{
+  document.getElementById("resultado").innerHTML = ("Não há");
+}
+}
