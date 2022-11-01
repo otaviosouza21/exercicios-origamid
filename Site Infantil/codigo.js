b6bcfb631f25ef66b0b89a5f34c130224c0d47a3
Otavio Souza <otaviosouzalu@gmail.com>
2022-11-01 06:35:16 -0300

    Exercicio novo

---
+++ Site Infantil/codigo.js
@@ -0,0 +1,104 @@
+const botao1 = document.querySelector(".checkbox1");
+console.log(botao1);
+
+function selecionar1() {
+  botao1.classList.toggle("clicado");
+}
+botao1.addEventListener("click",selecionar1);
+
+
+const botao2 = document.querySelector(".checkbox2");
+console.log(botao2)
+
+function selecionar2() {
+  botao2.classList.toggle("clicado");
+}
+botao2.addEventListener("click",selecionar2);
+
+const botao3 = document.querySelector(".checkbox3");
+console.log(botao3)
+
+function selecionar3() {
+  botao3.classList.toggle("clicado");
+}
+botao3.addEventListener("click",selecionar3);
+
+const botao4 = document.querySelector(".checkbox4");
+console.log(botao4)
+
+function selecionar4() {
+  botao4.classList.toggle("clicado");
+}
+botao4.addEventListener("click",selecionar4);
+
+const botao5 = document.querySelector(".checkbox5");
+console.log(botao5)
+
+function selecionar5() {
+  botao5.classList.toggle("clicado");
+}
+botao5.addEventListener("click",selecionar5);
+
+const botao6 = document.querySelector(".checkbox6");
+console.log(botao6)
+
+function selecionar6() {
+  botao6.classList.toggle("clicado");
+}
+botao6.addEventListener("click",selecionar6);
+
+const botao7 = document.querySelector(".checkbox7");
+console.log(botao7)
+
+function selecionar7() {
+  botao7.classList.toggle("clicado");
+}
+botao7.addEventListener("click",selecionar7);
+
+
+const botao8 = document.querySelector(".checkbox8");
+console.log(botao8)
+
+function selecionar8() {
+  botao8.classList.toggle("clicado");
+}
+botao8.addEventListener("click",selecionar8);
+
+
+const botao9 = document.querySelector(".checkbox9");
+console.log(botao9)
+
+function selecionar9() {
+  botao9.classList.toggle("clicado");
+}
+botao10.addEventListener("click",selecionar10);
+
+
+
+const botao10 = document.querySelector(".checkbox10");
+console.log(botao10)
+
+function selecionar10() {
+  botao10.classList.toggle("clicado");
+}
+botao10.addEventListener("click",selecionar10);
+
+
+
+const botao11 = document.querySelector(".checkbox11");
+console.log(botao11)
+
+function selecionar11() {
+  botao11.classList.toggle("clicado");
+}
+botao11.addEventListener("click",selecionar11);
+
+
+
+const botao12 = document.querySelector(".checkbox12");
+console.log(botao12)
+
+function selecionar12() {
+  botao12.classList.toggle("clicado");
+}
+botao12.addEventListener("click",selecionar12);
