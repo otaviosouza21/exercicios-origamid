5885384bb86637c85378abd58624dcf9ae70bc43
Otavio Souza <otaviosouzalu@gmail.com>
2022-12-19 23:08:40 -0300

    publish news site

---
+++ News-homepage/script/script.js
@@ -0,0 +1,9 @@
+$(".icon-menu-burguer").click(function(){
+$(".navegaçao-mobile").show(400);
+$(".blackout").show();
+})
+
+$(".xis").click(function(){
+  $(".navegaçao-mobile").hide(400);
+  $(".blackout").hide();
+})
